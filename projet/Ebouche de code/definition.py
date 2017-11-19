3355f38c084e1ef84181993f91b849ce462f9f31
ElieGrnr <elie.grenier@gmail.com>
2017-11-19 22:56:04 +0100

    des précisions sur un brouillons (qui n'est juste qu'un essai sans prétention) alors qu'il est 23h!

--- projet/Ebouche de code/definition.py
+++ projet/Ebouche de code/definition.py
@@ -1,19 +1,19 @@
-class Piece:
+class Piece: # cette classe définit ce qu'est une pièce du jeu
     
     def __init__(self, color, height, shape, matter):
-        self.color = color
-        self.height = height
-        self.shape = shape
-        self.matter = matter
+        self.color = color # couleur : clair/foncé (o/1 par exemple)
+        self.height = height # hauteur : gtand/petit (0/1 par ex)
+        self.shape = shape # rond/carré (0/1 par ex )
+        self.matter = matter # creux ou plein (idem)
         
-class Game(Piece):
+class Game(Piece): # cette classe définit ce qu'est une situation de jeux (un étape, une configuration du plateau à un moment donné ...)
     
     def __init__(self):
-        self.set = [[None, None,None][None, None,None],[None, None,None]]
-        self.bag = {}
+        self.set = [[],[],[],[]] # ceci est le plateau du jeu 4*4 avec aucune pièce dessus (ne fonctionne pas commen ça mais vous comprenez l'idée)
+        self.bag = {}# les pièces qui sont sur le côté du jeu donc pas sur le plateau (dans un sac en fait). Chaque pièce étant unique, on attribue à achacune un idéntifiant id, d'où l'idée d'un dictionnaire où les clés sont les identifiants.
         
-    def add(self, id_piece, coord):
+    def add(self, id_piece, coord): # méthode qui ajoute une pièce reconnue par son identifiant sur le plateau à la case de coordonée coord ( couple)
         self.set[coord[0]][coord[1]] = self.bag[id_piece]
-        del self.bag[id_piece]
+        del self.bag[id_piece] # on enlève la pièce du sac car elle est sur le plateau
         
         
