16e134d39b8db36eeebb8a8ef5a48ab6e5de8b0d
ElieGrnr <elie.grenier@gmail.com>
2017-11-22 09:40:47 +0100

    premier test de classe, sans commentaire

--- projet/code/game.py
+++ projet/code/game.py
@@ -9,4 +9,47 @@ class Game: # cette classe définit ce qu'est une situation de jeux (un étape, 
             coord):  # méthode qui ajoute une pièce reconnue par son identifiant sur le plateau à la case de coordonée coord ( couple)
         self.set[coord[0]][coord[1]] = self.bag[id_piece]
         del self.bag[id_piece]  # on enlève la pièce du sac car elle est sur le plateau
+        
+
+
+
+
+
+
+### test         
+
+import numpy as np
+
+        
+class Piece:
+     
+    def __init__(self, descr):
+         self.descr = descr
+         
+    def __repr__(self):
+        print(self.descr)
+        
+class Game(Piece):
+    
+    def __init__(self, n):
+        self.set = np.zeros((n,n))
+        self.bag = {}
+        self.player = "PLAYER A"
+        self.win = False
+        self.end = False
+        self.winner = None
+    
+    def __repr__(self):
+        layer = [np.zeros((n,n))*n]
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    layer[k][i][j] = self.set[i][j].descr[k] 
+        for k in range(n):
+            print("caractéristique  ",k, " : ")
+            print(layer[k])
+   
+    
+         
+        
 
