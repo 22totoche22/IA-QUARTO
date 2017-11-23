7c0abfe88fb986432d0b92cc2b1c99e20736a973
DenisAll <allandenis974@gmail.com>
2017-11-23 23:38:46 +0100

    ajout du cas d'erreur lors du placement d'une pièce sur le plateau

--- projet/code/game.py
+++ projet/code/game.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 
 import numpy as np
 
 PLAYER_A = 1
 PLAYER_B = 0
 SIZE = 4
+
+
+class Bag_error(Exception):
+    pass
+
+class Cell_error(Exception):
+    pass
 
 class Piece:
      
@@ -23,6 +32,7 @@ class Game:
         self.player = PLAYER_A
         self.win = False
         self.end = False
+        self.coords = [(i,j) for i in range(self.size) for j in range(self.size)] #list of all possibles coordinates
 
 
     def full_bag(self):   #initializes the bag at the beginning of the game
@@ -35,9 +45,26 @@ class Game:
 
     def play_piece(self,num_piece,coord): #adds a object Piece from the bag  on the board at the coordinates (x,y) and remove it from the bag, coord is a tuple
         x,y = coord[0], coord[1]
+
         for k in range(self.size):
-            self.board[x][y][k] = self.bag[num_piece].charact[k]              #adds the Piece on the board
+            try:
+                self.board[x][y][k] = self.bag[num_piece].charact[k]
+            except Exception: #Error if self.bag[num_piece] doesn't exist anymore
+                print(Bag_error("this piece as already been played: Choose an other one"))
+                num_piece = int(input("num_piece?"))
+                self.board[x][y][k] = self.bag[num_piece].charact[k] #adds the Piece on the board
+
+            try:
+                coord = self.coords[self.coords.index(coord)]
+            except Exception: #Error if the coordinates "coord" have already been choosen 
+                print(Cell_error("this cell is already taken: Choose an other ones"))
+                coord = tuple(input("coord?"))
+                self.board[x][y][k] = self.bag[num_piece].charact[k] #adds the Piece on the board
+
+        
+        del self.coords[self.coords.index(coord)]
         del self.bag[num_piece]                             #removes the Piece from the bag
+            
 
 #rajouter erreur si pièce plus dans le sac,...
 
@@ -83,21 +110,47 @@ def row_layer(layer,n,coord): #returns if there is a horizontal, vertical, or di
     return victory
 
 
+    
+piece = Piece([0,1,1,0])
 jeu = Game(SIZE)
 jeu.full_bag()
-print(jeu.board)
-print(jeu.bag)
 
-jeu.play_piece(2,(0,2))
-print(jeu.full_row((0,2),SIZE))
-jeu.play_piece(6,(1,2))
-print(jeu.full_row((1,2),SIZE))
-jeu.play_piece(10,(2,2))
-print(jeu.full_row((2,2),SIZE))
-jeu.play_piece(11,(3,2))
-print(jeu.full_row((3,2),SIZE))
+
+
+jeu.play_piece(2,(2,3))
+jeu.play_piece(2,(1,3))
 print(jeu.board)
 print(jeu.bag)
+print(jeu.coords)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
