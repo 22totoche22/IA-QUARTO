3d544e6cff43fd325ad60acc7cec28c049746ba5
22totoche22 <adrien.caillet2@gmail.com>
2018-01-19 17:16:02 +0100

    rien à faire, final ?

--- projet/code/main.py
+++ projet/code/main.py
@@ -1,9 +1,19 @@
-
 # -*- coding: utf-8 -*-
-
 import time
 import game
 import ia
+
+
+
+class Bag_error(Exception):
+    pass
+
+
+class Cell_error(Exception):
+    pass
+
+
+
 from collections import deque
 from random import randrange
 
@@ -14,9 +24,40 @@ if __name__ == "__main__":
     # deque([(None, 3), ((1, 2), 15), ((3, 3), 13), ((2, 2), 7), ((3, 2), 11)])
     ## DEBUT MAIN AVEC IA
     # launched_game.select_piece(randrange(launched_game.size))
-    launched_game.select_piece(0)
+    error = False
+    launched_game.current_player = int(input("{:=^50}".format(" choisissez le premier joueur : ")
+                                            +("\n\n")
+                                            +"{:=^50}".format(" joueur 1 ou joueur -1 ? ")
+                                            +("\n\n")))
+    compteur = 0
+    while error == False:
+        if launched_game.current_player == 1:
+            num = int(input(("choisissez la première pièce : ")))
+            break
+        
+        elif launched_game.current_player == -1:
+            num = randrange(game.SIZE**2)
+            break
+        
+        else: # Traitement du cas où le joueur est trop con et n'est pas foutu de taper "1" ou "-1"
+            launched_game.current_player = int(input("{:=^50}".format(" vous vous êtes trompé : ")
+                                            +("\n\n")
+                                            +"{:=^50}".format(" recommencez : ")
+                                            +("\n\n")))
+            compteur += 1
+            if compteur >= 5:
+                launched_game.current_player = int(input("{:=^50}".format(" c'est bon t'as fini de rigoler ? ")
+                                            +("\n\n")
+                                            +"{:=^50}".format(" tape 1 ou -1 bordel ! ")
+                                            +("\n\n")))
+                
+        
+        
+        
+    launched_game.select_piece(num)
     launched_game.turns_played = deque([(None, launched_game.selected_piece.num)])
     # launched_game.init_from_turns_played([(None, 3), ((1, 2), 15)])
+    launched_game.current_player *= -1
     while not launched_game.end:
         if launched_game.current_player == 1:
 
@@ -34,18 +75,38 @@ if __name__ == "__main__":
                 print(el)
             # TODO: si le joueur ecrit quarto! vérifier qu'il a gagné
             i = int(input("Numéro de la pièce = "))
+            
+                
 
             launched_game.play_turn((x, y), i)
-        else:
+        elif launched_game.current_player == -1:
             print("\n\n{:=^50}".format(" Tour de Charles-Maurice "))
             t1 = time.time()
-            # TODO: augmenter la profondeur lorsqu'on arrive vers une grille plus remplie
+            depth = 0
+            
+           
+            if len(launched_game.bag) == launched_game.size**2 - 1:
+                        num_piece = randrange(launched_game.size**2 - 1)
+                        while num_piece == launched_game.selected_piece:
+                            num_piece = randrange(launched_game.size**2 - 1)
+                        coord = ((randrange(launched_game.size), randrange(launched_game.size)))
+                        
 
-            ((coord, num_piece), v) = ia.alphabeta(launched_game, 3)
-            # ((coord, num_piece), v) = ia.minimax(launched_game, 4)
-            print(coord, num_piece)
-
+                   
+            
+            elif len(launched_game.bag) >= 2**launched_game.size - launched_game.size:
+                ((coord, num_piece), v) = ia.alphabeta(launched_game, 2)
+                
+            elif 2**launched_game.size - launched_game.size > len(launched_game.bag) >= 6 :
+                ((coord, num_piece), v) = ia.alphabeta(launched_game, 3)
+                
+            else :# len(launched_game.bag) <= launched_game.size :
+                ((coord, num_piece), v) = ia.alphabeta(launched_game, 6)
+            print (coord, num_piece)
+            
+            
             launched_game.play_turn(coord, num_piece)
+                
 
             print(time.time()-t1)
 
